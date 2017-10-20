9eb139bdf94b4d999d0dca956b7625afe9d9b0e0
Jay R Bolton <jayrbolton@gmail.com>
2017-10-20 13:15:11 -0700

    Remove node_modules oops lol

--- node_modules/bel/bel.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "bel" {
-    export default function (strings: TemplateStringsArray, ...keys): HTMLElement;
-    export function createElement (tag: string, attributes: any, children: Array<any>): HTMLElement;
-}
