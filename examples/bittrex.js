d1a2faba5c9265062f7ce95a93a66bf367cf09fc
Doug Wright <mrdougwright@gmail.com>
2017-10-31 10:24:05 -0700

    get bittrex api example working

--- examples/bittrex.js
+++ examples/bittrex.js
@@ -1,40 +1,43 @@
-const state = require('../index')
 const html = require('bel')
+const state = require('../index')
+const dom = require('../dom')
 
-function BittrexData (initial) {
-  return state({ data: initial })
-}
+const BittrexData = (initial) => state({ bitMarkets: initial })
 
-function apiCall () {
-  return fetch("https://bittrex.com/api/v1.1/public/getmarketsummaries", {})
+const apiCall = () =>
+  fetch("https://bittrex.com/api/v1.1/public/getmarketsummaries", {})
     .then(res => res.json())
     .then(obj => obj.result)
     .then(data => data)
-}
 
 const responseHeaders = ["Market Name", "Volume", "Bid", "Open Buy Orders", "Created"]
-const concatWord = (word) => word.split(" ").reduce((a,b) => a.concat(b))
+const concatWord = (word) => word.replace(/ /g, '')
+const apiAttrs = responseHeaders.map(concatWord)
 
-const renderHeader = (value) => html`<th>${value}</th>`
-const renderRow = (data, key) => html`<tr>${data[key]}</tr>`
+const headerView = (value) => html`<th>${value}</th>`
+const rowView = (row, bd) =>
+  html`<tr>${apiAttrs.map(key =>
+    html`<td>${row[key]}</td>`
+  )}</tr>`
 
-function view (bd) {
-  apiCall().then( data => bd.update({ data }) )
+const view = (bd) => {
+  apiCall().then(data =>
+    data.map((market, index) =>
+      Object.assign({id: `mkt-${index}`}, market)
+    )
+  ).then(cleanData => bd.update({bitMarkets: cleanData}))
 
-  bd.on('data', (b) => {
-    console.log(b[0])
-  })
+  const rows = dom.childSync(
+    (row) => rowView(row, bd),
+    document.createElement('tbody'),
+    bd,
+    'bitMarkets',
+  )
 
   return html`
     <table>
-      <tbody>
-        <thead>${responseHeaders.map(renderHeader)}</thead>
-        <div>${
-          responseHeaders
-            .map(concatWord)
-            .map(h => renderRow(bd.data, h))
-        }</div>
-      </tbody>
+      <thead>${responseHeaders.map(headerView)}</thead>
+      ${rows}
     </table>
   `
 }
